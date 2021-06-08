771b9ae15b2314bbc67a2194827e85378410a04b
Anirudh Sanjeev <skyronic@gmail.com>
2021-06-08 11:28:22 -0400

    Add syntax tests

---
+++ syntax_test_basic.vue
@@ -0,0 +1,34 @@
+<!-- SYNTAX TEST "Packages/User/vue-syntax-highlight/Vue Component.sublime-syntax" -->
+
+<template>
+	<a @on.click="foo()">Click Me</a>
+<!-- ^ text.html.vue -->
+<!--              ^ source.js -->
+    <span v-text="foo * 10 + 'hi'" :id="foo + 'baz'">Hello {{ world }}</span>
+<!--              ^ source.js -->
+<!--                          ^ meta.string.js -->
+<!--                                     ^ source.js -->
+<!--                                            ^ meta.string.js -->
+<!--                                                           ^ source.js -->
+
+</template>
+
+<script>
+	export default {
+	// ^ source.js
+	// ^ meta.export.js
+
+		function () {
+
+		}
+	}
+</script>
+
+<style>
+	body {
+	/* ^ source.css */
+		background-color: yellow;
+	/*                     ^ meta.block.css */
+	/*                     ^ meta.property.value.css */
+	}
+</style>
---
+++ syntax_test_langs.vue
@@ -0,0 +1,41 @@
+<!-- SYNTAX TEST "Packages/User/vue-syntax-highlight/Vue Component.sublime-syntax" -->
+
+<style lang="stylus">
+font-stack = Helvetica, sans-serif
+// ^ source.stylus
+primary-color = #999
+body
+  font 100% font-stack
+  color primary-color
+</style>
+
+<style lang="scss">
+@import '~foo';
+// ^ source.scss
+
+a {
+  color: red;
+}
+</style>
+
+<style lang="sass">
+@import '~foo'
+// ^ source.sass
+a
+  color: red
+</style>
+
+<template lang="pug">
+div.app
+  h1.title This is the app
+  // ^ source.pug
+  comp-a(foo="bar", :a=1)
+  comp-b(bar="baz", :a=234)
+</template>
+
+<script lang="coffee">
+module.exports =
+  data: ->
+    msg: 'Hello from coffee!'
+    # ^ source.coffee
+</script>
