dbe9f0a82549c2d75970cd50d0bf1414867106e8
DeathAxe <deathaxe82@googlemail.com>
2021-09-08 21:20:21 +0200

    Fix meta scopes of template tags
    This commit...
    
    1. fixes duplicated meta.tag scopes in `<template>` tags
    2. fixes missing meta.tag scope at the closing `>` in `<template>` tags
    3. adds syntax tests for template tags

---
+++ tests/syntax_test_template.vue
@@ -0,0 +1,101 @@
+// SYNTAX TEST "Vue Component.sublime-syntax"
+
+
+    <template lang="jade"> foo </template>
+//  ^^^^^^^^^^^^^^^^^^^^^^ meta.tag - source
+//                        ^^^^^ text.jade.embedded.html - meta.tag
+//                             ^^^^^^^^^^^ meta.tag - source
+
+    <template lang="jade"> foo --> </template>
+//  ^^^^^^^^^^^^^^^^^^^^^^ meta.tag - source
+//                        ^^^^^ text.jade.embedded.html - meta.tag
+//                             ^^^^ - meta.tag - source
+//                             ^^^ comment.block.html punctuation.definition.comment.end.html
+//                                 ^^^^^^^^^^^ meta.tag - source
+
+    <template lang="jade"> <!-- foo </template>
+//  ^^^^^^^^^^^^^^^^^^^^^^ meta.tag - source
+//                        ^^^^^ - meta.tag - source
+//                         ^^^^ punctuation.definition.comment.begin.html
+//                             ^^^^^ text.jade.embedded.html - meta.tag
+//                                  ^^^^^^^^^^^ meta.tag - source
+
+    <template lang="jade"> <!-- foo --> </template>
+//  ^^^^^^^^^^^^^^^^^^^^^^ meta.tag - source
+//                        ^^^^^ - meta.tag - source
+//                         ^^^^ punctuation.definition.comment.begin.html
+//                             ^^^^^ text.jade.embedded.html - meta.tag
+//                                  ^^^^ - meta.tag - source
+//                                  ^^^ comment.block.html punctuation.definition.comment.end.html
+//                                      ^^^^^^^^^^^ meta.tag - source
+
+
+    <template lang="jade">
+
+// <- text.jade.embedded.html
+
+    </template>
+// <- text.jade.embedded.html
+//^^ text.jade.embedded.html
+//  ^^^^^^^^^^^ meta.tag - meta.tag meta.tag - text.jade
+
+
+    <template lang="jade">
+//  ^^^^^^^^^^ meta.tag - meta.tag meta.tag - meta.attribute-with-value
+//            ^^^^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag
+//                       ^ meta.tag.template.begin.html - meta.tag meta.tag
+//  ^ punctuation.definition.tag.begin.html
+//   ^^^^^^^^ entity.name.tag.template.html
+//            ^^^^ entity.other.attribute-name.html
+//                ^ punctuation.separator.key-value.html
+//                       ^ punctuation.definition.tag.end.html
+//                        ^ - text.jade
+
+// <- text.jade.embedded.html
+    </template>
+// <- text.jade.embedded.html
+//^^ text.jade.embedded.html
+//  ^^^^^^^^^^^ meta.tag - meta.tag meta.tag - text.jade
+//  ^^ punctuation.definition.tag.begin.html
+//    ^^^^^^^^ entity.name.tag.template.html
+//            ^ punctuation.definition.tag.end.html
+
+    <template lang="pug">
+//  ^^^^^^^^^^ meta.tag - meta.tag meta.tag - meta.attribute-with-value
+//            ^^^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag
+//                      ^ meta.tag.template.begin.html - meta.tag meta.tag
+//  ^ punctuation.definition.tag.begin.html
+//   ^^^^^^^^ entity.name.tag.template.html
+//            ^^^^ entity.other.attribute-name.html
+//                ^ punctuation.separator.key-value.html
+//                      ^ punctuation.definition.tag.end.html
+//                       ^ - text.pug
+
+// <- text.pug.embedded.html
+    </template>
+// <- text.pug.embedded.html
+//^^ text.pug.embedded.html
+//  ^^^^^^^^^^^ meta.tag - meta.tag meta.tag - text.pug
+//  ^^ punctuation.definition.tag.begin.html
+//    ^^^^^^^^ entity.name.tag.template.html
+//            ^ punctuation.definition.tag.end.html
+
+    <template lang="slm">
+//  ^^^^^^^^^^ meta.tag - meta.tag meta.tag - meta.attribute-with-value
+//            ^^^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag
+//                      ^ meta.tag.template.begin.html - meta.tag meta.tag
+//  ^ punctuation.definition.tag.begin.html
+//   ^^^^^^^^ entity.name.tag.template.html
+//            ^^^^ entity.other.attribute-name.html
+//                ^ punctuation.separator.key-value.html
+//                      ^ punctuation.definition.tag.end.html
+//                       ^ - text.slm
+
+// <- text.slm.embedded.html
+    </template>
+// <- text.slm.embedded.html
+//^^ text.slm.embedded.html
+//  ^^^^^^^^^^^ meta.tag - meta.tag meta.tag - text.slm
+//  ^^ punctuation.definition.tag.begin.html
+//    ^^^^^^^^ entity.name.tag.template.html
+//            ^ punctuation.definition.tag.end.html
