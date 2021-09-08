f97fa7c57956a6ea6ea81580ec72d0855350a233
DeathAxe <deathaxe82@googlemail.com>
2021-09-08 18:15:20 +0200

    Add tests for script/style tags

---
+++ tests/syntax_test_script.vue
@@ -0,0 +1,200 @@
+// SYNTAX TEST "Packages/Vue/Vue Component.sublime-syntax"
+
+
+    <script> var i = 0; </script>
+//  ^^^^^^^^ meta.tag - source
+//          ^^^^^^^^^^^^ source.js.embedded.html - meta.tag
+//                      ^^^^^^^^^ meta.tag - source
+
+    <script> var i = 0; --> </script>
+//  ^^^^^^^^ meta.tag - source
+//          ^^^^^^^^^^^^ source.js.embedded.html - meta.tag
+//                      ^^^^ - meta.tag - source
+//                      ^^^ comment.block.html punctuation.definition.comment.end.html
+//                          ^^^^^^^^^ meta.tag - source
+
+    <script> <!-- var i = 0; </script>
+//  ^^^^^^^^ meta.tag - source
+//          ^^^^^ - meta.tag - source
+//               ^^^^^^^^^^^^ source.js.embedded.html - meta.tag
+//                           ^^^^^^^^^ meta.tag - source
+//           ^^^^ punctuation.definition.comment.begin.html
+
+    <script> <!-- var i = 0; --> </script>
+//  ^^^^^^^^ meta.tag - source
+//          ^^^^^ - meta.tag - source
+//           ^^^^ punctuation.definition.comment.begin.html
+//               ^^^^^^^^^^^^ source.js.embedded.html - meta.tag
+//                           ^^^^ - meta.tag - source
+//                           ^^^ comment.block.html punctuation.definition.comment.end.html
+//                               ^^^^^^^^^ meta.tag - source
+
+
+    <script>
+
+// <- source.js.embedded.html
+    var i = 0;
+// ^^^^^^^^^^^^ source.js.embedded.html - source source
+    </script>
+// <- source.js.embedded.html - source source
+// ^ source.js.embedded.html - source source
+//  ^^^^^^^^^ meta.tag - source
+
+
+    <script>
+    <!--
+//^^^^^^ - meta.tag - source
+//  ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//      ^ source.js.embedded.html - source source - meta.tag - comment
+    var i = 0;
+// ^^^^^^^^^^^^ source.js.embedded.html - source source - meta.tag
+    -->
+//^^ source.js.embedded.html - meta.tag - comment
+//  ^^^^ - source - meta.tag
+//  ^^^ comment.block.html punctuation.definition.comment.end.html
+    var i = 0;
+// ^^^^^^^^^^^^ - source
+    </script>
+//  ^^^^^^^^^ meta.tag - source
+
+
+    <script> <!--
+//  ^^^^^^^^^^^^^ - source
+//  ^^^^^^^^ meta.tag
+//  ^ punctuation.definition.tag.begin.html
+//   ^^^^^^ entity.name.tag
+//         ^ punctuation.definition.tag.end.html
+//          ^ - meta.tag - comment
+//           ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//               ^ source.js.embedded.html - source source
+
+    var foo = 100;
+// <- source.js.embedded.html - source source
+
+    (a --> b);
+//     ^^^ source.js.embedded.html keyword.operator - comment
+
+    --> </script>
+// ^ source.js.embedded.html - source source
+//  ^^^^^^^^^^^^^^ - source
+//     ^ - meta.tag - comment
+//      ^^^^^^^^^ meta.tag
+//               ^ - meta.tag
+//  ^^^ comment.block.html punctuation.definition.comment.end.html
+//      ^^ punctuation.definition.tag.begin.html
+//        ^^^^^^ entity.name.tag
+//              ^ punctuation.definition.tag.end.html
+
+
+    <script type="text/javascript"> <!--
+//  ^^^^^^^^ meta.tag - meta.tag meta.tag - meta.attribute-with-value - source
+//          ^^^^^^^^^^^^^^^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag - meta.attribute-with-value meta.attribute-with-value - source
+//                                ^ meta.tag - meta.tag meta.tag - meta.attribute-with-value - source
+//                                 ^^^^^ - meta.tag - source
+//   ^^^^^^ entity.name.tag
+//          ^^^^ entity.other.attribute-name.html
+//              ^ punctuation.separator.key-value.html
+//               ^^^^^^^^^^^^^^^^^ string.quoted.double.html
+//                                  ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//                                      ^ source.js.embedded.html
+
+    var foo = 100;
+// <- source.js.embedded.html - source source
+
+    (a --> b);
+//     ^^^ source.js.embedded.html keyword.operator - comment
+
+    --> </script>
+// ^ source.js.embedded.html - source source
+//  ^^^^^^^^^^^^^^ - source
+//     ^ - meta.tag - comment
+//      ^^^^^^^^^ meta.tag
+//               ^ - meta.tag
+//  ^^^ comment.block.html punctuation.definition.comment.end.html
+//      ^^ punctuation.definition.tag.begin.html
+//        ^^^^^^ entity.name.tag
+//              ^ punctuation.definition.tag.end.html
+
+
+    <script
+    type
+//  ^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag - meta.attribute-with-value meta.attribute-with-value
+    =
+    application/jAvAsCrIpT>
+//  ^^^^^^^^^^^^^^^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag - meta.attribute-with-value meta.attribute-with-value
+    var foo = 100;
+// <- source.js.embedded.html - source source
+// ^^^^^^^^^^^^^^^^ source.js.embedded.html - source source
+    </script>
+// <- source.js.embedded.html - source source
+// ^ source.js.embedded.html - source source
+//  ^^^^^^^^^ meta.tag - source
+
+
+    <script lang="coffee">
+
+// <- source.coffee.embedded.html - source source
+    </script>
+// <- source.coffee.embedded.html - source source
+//^^ source.coffee.embedded.html - source source
+//  ^^^^^^^^^ meta.tag - source
+
+    <script lang="coffee">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.coffee.embedded.html - source source
+        -->
+// ^^^^^ source.coffee.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </script>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^^ meta.tag - source
+
+
+    <script lang="livescript">
+
+// <- source.livescript.embedded.html - source source
+    </script>
+// <- source.livescript.embedded.html - source source
+//^^ source.livescript.embedded.html - source source
+//  ^^^^^^^^^ meta.tag - source
+
+    <script lang="livescript">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.livescript.embedded.html - source source
+        -->
+// ^^^^^ source.livescript.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </script>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^^ meta.tag - source
+
+
+    <script lang="ts">
+
+// <- source.ts.embedded.html - source source
+    </script>
+// <- source.ts.embedded.html - source source
+//^^ source.ts.embedded.html - source source
+//  ^^^^^^^^^ meta.tag - source
+
+    <script lang="ts">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.ts.embedded.html - source source
+        -->
+// ^^^^^ source.ts.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </script>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^^ meta.tag - source
---
+++ tests/syntax_test_style.vue
@@ -0,0 +1,240 @@
+// SYNTAX TEST "Packages/Vue/Vue Component.sublime-syntax"
+
+
+    <style> h1 {} </style>
+//  ^^^^^^^ meta.tag - source
+//         ^^^^^^^ source.css.embedded.html - meta.tag
+//                ^^^^^^^^ meta.tag - source
+
+    <style> h1 {} --> </style>
+//  ^^^^^^^ meta.tag - source
+//         ^^^^^^^ source.css.embedded.html - meta.tag
+//                ^^^^ - meta.tag - source
+//                ^^^ comment.block.html punctuation.definition.comment.end.html
+//                    ^^^^^^^^ meta.tag - source
+
+    <style> <!-- h1 {} </style>
+//  ^^^^^^^ meta.tag - source
+//         ^^^^^ - meta.tag - source
+//              ^^^^^^^ source.css.embedded.html - meta.tag
+//                     ^^^^^^^^ meta.tag - source
+//          ^^^^ punctuation.definition.comment.begin.html
+
+    <style> <!-- h1 {} --> </style>
+//  ^^^^^^^ meta.tag - source
+//         ^^^^^ - meta.tag - source
+//          ^^^^ punctuation.definition.comment.begin.html
+//              ^^^^^^^ source.css.embedded.html - meta.tag
+//                     ^^^^ - meta.tag - source
+//                     ^^^ comment.block.html punctuation.definition.comment.end.html
+//                         ^^^^^^^^ meta.tag - source
+
+
+    <style>
+
+// <- source.css.embedded.html
+
+    </style>
+// <- source.css.embedded.html - source source
+// ^ source.css.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style>
+    <!--
+//^^^^^^ - meta.tag - source
+//  ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//      ^ source.css.embedded.html - source source - meta.tag - comment
+    h1 {}
+// ^^^^^^^ source.css.embedded.html - source source - meta.tag
+    -->
+//^^ source.css.embedded.html - meta.tag - comment
+//  ^^^^ - source - meta.tag
+//  ^^^ comment.block.html punctuation.definition.comment.end.html
+    var i = 0;
+// ^^^^^^^^^^^^ - source
+    </style>
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style> <!--
+//  ^^^^^^^^^^^^ - source
+//  ^^^^^^^ meta.tag
+//  ^ punctuation.definition.tag.begin.html
+//   ^^^^^ entity.name.tag
+//        ^ punctuation.definition.tag.end.html
+//         ^ - meta.tag - comment
+//          ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//              ^ source.css.embedded.html - source source
+
+    h1 {}
+// <- source.css.embedded.html - source source
+
+    --> </style>
+// ^ source.css.embedded.html - source source
+//  ^^^^^^^^^^^^^ - source
+//     ^ - meta.tag - comment
+//      ^^^^^^^^ meta.tag
+//              ^ - meta.tag
+//  ^^^ comment.block.html punctuation.definition.comment.end.html
+//      ^^ punctuation.definition.tag.begin.html
+//        ^^^^^ entity.name.tag
+//             ^ punctuation.definition.tag.end.html
+
+
+    <style type="text/css"> <!--
+//  ^^^^^^^ meta.tag - meta.tag meta.tag - meta.attribute-with-value - source
+//         ^^^^^^^^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag - meta.attribute-with-value meta.attribute-with-value - source
+//                        ^ meta.tag - meta.tag meta.tag - meta.attribute-with-value - source
+//                         ^^^^^ - meta.tag - source
+//   ^^^^^ entity.name.tag
+//         ^^^^ entity.other.attribute-name.html
+//             ^ punctuation.separator.key-value.html
+//              ^^^^^^^^^^ string.quoted.double.html
+//                          ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//                              ^ source.css.embedded.html
+
+    h1 {}
+// <- source.css.embedded.html - source source
+
+    --> </style>
+// ^ source.css.embedded.html - source source
+//  ^^^^^^^^^^^^^ - source
+//     ^ - meta.tag - comment
+//      ^^^^^^^^ meta.tag
+//              ^ - meta.tag
+//  ^^^ comment.block.html punctuation.definition.comment.end.html
+//      ^^ punctuation.definition.tag.begin.html
+//        ^^^^^ entity.name.tag
+//             ^ punctuation.definition.tag.end.html
+
+
+    <style
+    type
+//  ^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag - meta.attribute-with-value meta.attribute-with-value
+    =
+    tExT/cSs>
+//  ^^^^^^^^ meta.tag meta.attribute-with-value.html - meta.tag meta.tag - meta.attribute-with-value meta.attribute-with-value
+
+    h1 {}
+// <- source.css.embedded.html - source source
+
+    </style>
+// <- source.css.embedded.html - source source
+// ^ source.css.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style lang="less">
+
+// <- source.less.embedded.html - source source
+    </style>
+// <- source.less.embedded.html - source source
+//^^ source.less.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+    <style lang="less">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.less.embedded.html - source source
+        -->
+// ^^^^^ source.less.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </style>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style lang="postcss">
+
+// <- source.postcss.embedded.html - source source
+    </style>
+// <- source.postcss.embedded.html - source source
+//^^ source.postcss.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+    <style lang="postcss">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.postcss.embedded.html - source source
+        -->
+// ^^^^^ source.postcss.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </style>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style lang="postcss?parser=sugarss">
+
+// <- source.sss.embedded.html - source source
+    </style>
+// <- source.sss.embedded.html - source source
+//^^ source.sss.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+    <style lang="postcss?parser=sugarss">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.sss.embedded.html - source source
+        -->
+// ^^^^^ source.sss.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </style>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style lang="sass">
+
+// <- source.sass.embedded.html - source source
+    </style>
+// <- source.sass.embedded.html - source source
+//^^ source.sass.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+    <style lang="sass">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.sass.embedded.html - source source
+        -->
+// ^^^^^ source.sass.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </style>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^ meta.tag - source
+
+
+    <style lang="scss">
+
+// <- source.scss.embedded.html - source source
+    </style>
+// <- source.scss.embedded.html - source source
+//^^ source.scss.embedded.html - source source
+//  ^^^^^^^^ meta.tag - source
+
+    <style lang="scss">
+        <!--
+// ^^^^^^^^^ - source
+//      ^^^^ comment.block.html punctuation.definition.comment.begin.html
+//          ^ source.scss.embedded.html - source source
+        -->
+// ^^^^^ source.scss.embedded.html - source source
+//      ^^^ comment.block.html punctuation.definition.comment.end.html - source
+//         ^ - source - comment - meta.tag
+    </style>
+// <- - source - comment - meta.tag
+//^^ - source - comment - meta.tag
+//  ^^^^^^^^ meta.tag - source
