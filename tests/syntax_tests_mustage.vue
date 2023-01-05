b083a278bbf4aa6990d43c2ffb389dc68eb9654d
deathaxe <deathaxe82@googlemail.com>
2023-01-05 17:59:22 +0100

    Drop meta.template
    This commit follows a scope naming scheme introduced to Astro, Blade, Liquid, PHP and Slim recently, which uses
    
    1. `meta.embedded.<template-name>` for those template code sections,
       which are used for control structures primarily.
    
       This scope has already been in use in PHP syntax to scope all
       `<?php ... ?>` code sections.
    
    2. `meta.interpolation.<template-name>` for those template code
        sections, which primarily emit data to the output document.
    
        That's what Mustage tags `{{ expr }}` do primarily.

--- tests/syntax_tests_mustage.vue
+++ tests/syntax_tests_mustage.vue
@@ -1,25 +1,25 @@
 // SYNTAX TEST "Vue Component.sublime-syntax"
 
     <h1> {{ foo.text }} </h1>
-//       ^^^^^^^^^^^^^^ meta.template.vue
-//       ^^ punctuation.definition.template.begin.html
+//       ^^^^^^^^^^^^^^ meta.interpolation.vue
+//       ^^ punctuation.definition.interpolation.begin.html
 //         ^^^^^^^^^^ source.js.embedded.vue
-//                   ^^ punctuation.definition.template.end.html
+//                   ^^ punctuation.definition.interpolation.end.html
 
     <p {{foo.attrib}}>
-//     ^^^^^^^^^^^^^^ meta.template.vue
-//     ^^ punctuation.definition.template.begin.html
+//     ^^^^^^^^^^^^^^ meta.interpolation.vue
+//     ^^ punctuation.definition.interpolation.begin.html
 //       ^^^^^^^^^^ source.js.embedded.vue
-//                 ^^ punctuation.definition.template.end.html
+//                 ^^ punctuation.definition.interpolation.end.html
 
     <p attrib="{{ foo.value }}" >
 //            ^ meta.tag meta.attribute-with-value.html meta.string.html - meta.interpolation
 //             ^^^^^^^^^^^^^^^ meta.tag meta.attribute-with-value.html meta.string.html meta.interpolation.vue
 //                            ^ meta.tag meta.attribute-with-value.html meta.string.html - meta.interpolation
 //            ^ string.quoted.double.html punctuation.definition.string.begin.html
-//             ^^ punctuation.definition.template.begin.html - source.js
+//             ^^ punctuation.definition.interpolation.begin.html - source.js
 //               ^^^^^^^^^^^ source.js.embedded.vue
-//                          ^^ punctuation.definition.template.end.html - source.js
+//                          ^^ punctuation.definition.interpolation.end.html - source.js
 //                            ^ string.quoted.double.html punctuation.definition.string.end.html
 
     <p attrib='{{ foo.value }}' >
@@ -27,16 +27,16 @@
 //             ^^^^^^^^^^^^^^^ meta.tag meta.attribute-with-value.html meta.string.html meta.interpolation.vue
 //                            ^ meta.tag meta.attribute-with-value.html meta.string.html - meta.interpolation
 //            ^ string.quoted.single.html punctuation.definition.string.begin.html
-//             ^^ punctuation.definition.template.begin.html - source.js
+//             ^^ punctuation.definition.interpolation.begin.html - source.js
 //               ^^^^^^^^^^^ source.js.embedded.vue
-//                          ^^ punctuation.definition.template.end.html - source.js
+//                          ^^ punctuation.definition.interpolation.end.html - source.js
 //                            ^ string.quoted.single.html punctuation.definition.string.end.html
 
     <p attrib={{ foo.value }} >
 //            ^^^^^^^^^^^^^^^ meta.tag meta.attribute-with-value.html meta.string.html meta.interpolation.vue
-//            ^^ punctuation.definition.template.begin.html - source.js
+//            ^^ punctuation.definition.interpolation.begin.html - source.js
 //              ^^^^^^^^^^^ source.js.embedded.vue
-//                         ^^ punctuation.definition.template.end.html - source.js
+//                         ^^ punctuation.definition.interpolation.end.html - source.js
 
     <p v-attrib="{'key': 'value'}">
 //  ^^^ meta.tag - meta.attribute-with-value
